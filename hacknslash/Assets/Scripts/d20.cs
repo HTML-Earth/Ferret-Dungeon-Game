2ae9c4b90e7be40cf91e4b9651b95ba6061059f8
HTML-Earth <eak@HTML-Mac.local>
2015-04-30 22:56:07 +0200

    Removed ObjExporter script and added a comment

--- hacknslash/Assets/Scripts/d20.cs
+++ hacknslash/Assets/Scripts/d20.cs
@@ -53,7 +53,7 @@ public class d20 : MonoBehaviour
 		{
 			for (int i = 0; i < dSides.Length; i++)
 			{
-				d20_side d20side = dSides[i].GetComponent<d20_side>();
+				d20_side d20side = dSides[i].GetComponent<d20_side>(); //This comment was written on a Mac
 
 				dSides[i].GetComponent<Light>().enabled = true;
 			}
